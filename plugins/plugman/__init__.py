6891fc24ac45727c0f09892968c470d6798bc808
David Johnston <dwjgame11@gmail.com>
2021-02-06 15:14:39 -0500

    Update plugman
    Updated plugman to allow for the update command.

--- plugins/plugman/__init__.py
+++ plugins/plugman/__init__.py
@@ -17,6 +17,16 @@ def install(plugin_list: str, repository=None, update=False, **_):
         _logger.error("Installing from repositories is not supported at this time.")
 
 
+def update(plugin_list: str, repository=None, **_):
+    if repository is None:
+        if len(plugin_list) <= 0:
+            plugin_list = list(plugins.plugin_data.keys())
+        for plugin_name in plugin_list:
+            handlers.update(plugin_name, plugins.plugin_data[plugin], repository)
+    else:
+        _logger.error("Installing from repositories is not supported at this time.")
+
+
 def post_load_core():
     core = plugins.services["core"][0]
 
@@ -30,7 +40,7 @@ def post_load_terminal():
                                 help="Plugins to install.")
     install_parser.add_argument("-r",
                                 "--repository",
-                                help="Update the following plugins.",
+                                help="Repository to install from.",
                                 action="store_const",
                                 const=None)
     install_parser.add_argument("-u",
@@ -39,3 +49,16 @@ def post_load_terminal():
                                 action="store_true")
     terminal.create_command(install, "plugman.install", install_parser)
     terminal.create_command(install, "install", install_parser)
+
+    update_parser = terminal.ArgumentParser(prog="plugman.update",
+                                             description="Update plugins.",
+                                             exit_on_error=False)
+    update_parser.add_argument("plugin_list", nargs="*",
+                                help="Plugins to update.")
+    update_parser.add_argument("-r",
+                                "--repository",
+                                help="Repository to update from.",
+                                action="store_const",
+                                const=None)
+    terminal.create_command(update, "plugman.update", update_parser)
+    terminal.create_command(update, "update", update_parser)
--- plugins/plugman/handlers/apm/f0_1_0.py
+++ plugins/plugman/handlers/apm/f0_1_0.py
@@ -5,23 +5,21 @@ import os
 
 from ..... import plugins
 
+APPLE_DIRECTORY = plugins.APPLE_DIRECTORY
 PLUGIN_DIRECTORY = plugins.PLUGIN_DIRECTORY
 
 
 def install(plugin_manifest, plugin_manifest_file, plugin_remote_url, repository):
-    plugin_name = plugin_manifest["name"]
-    with open(os.path.join(PLUGIN_DIRECTORY, f"{plugin_name}.apm"), "wb") as plugin_local_file:
-        plugin_manifest_file.seek(0)
-        shutil.copyfileobj(plugin_manifest_file, plugin_local_file, -1)
     install_files(plugin_manifest)
 
 
 def install_files(plugin_manifest):
     for file in plugin_manifest["files"]:
-        local_url = file["local-url"]
+        local_url = os.path.join(APPLE_DIRECTORY, file["local-url"])
         remote_url = file["remote-url"]
         try:
-            os.makedirs(local_url)
+            os.makedirs(os.path.dirname(local_url))
         except FileExistsError:
             pass
+        print(remote_url, local_url)
         urllib.request.urlretrieve(remote_url, local_url)
